7a7d79008f55c4dc125e08fa076e75370d8be6d8
farlliant <160444686+farlliant@users.noreply.github.com>
2026-09-22 19:20:29 +0700

    feat: add ADT Double Linked List module experiments

---
+++ ADT Double Linked List/Percobaan-2-Tahap-1/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    Object data;
+    Node next;
+    Node prev;
+
+    public static void main(String[] args)
+    {
+        Node head = new Node();
+        head.data = "A";
+
+        System.out.println("data: " + head.data);
+        System.out.println("pointer: " + head.next);
+        System.out.println("pointer: " + head.prev);
+    }
+}
